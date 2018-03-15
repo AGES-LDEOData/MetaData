77d0aeccf8a268246518513401fe532fb56ee5f9
ages-ngx-admin <jake.ross@nmt.edu>
2018-03-15 18:35:42 -0400

    repo updated for analysis ba-01-N-09

--- scripts/ngx/ngx_close.py
+++ scripts/ngx/ngx_close.py
@@ -16,6 +16,7 @@ def main():
 #===============================================================================
 def main():
 	info('Pump after air analysis')
+	sleep(3)
 	open('D')
 	close('G')
 	open('A')
--- scripts/ngx/ngx_pump_air.py
+++ scripts/ngx/ngx_pump_air.py
@@ -1,5 +1,6 @@
 def main():
 	info('Pump after air analysis')
+	sleep(3)
 	open('D')
 	close('G')
 	open('A')
