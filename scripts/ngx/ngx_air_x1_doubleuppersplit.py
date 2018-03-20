a13ef623bef87c9efa521cefd3f9f6ffe4acf1c5
Stephen E Cox <cox.stephen.e@gmail.com>
2018-03-20 07:42:31 -0400

    repo updated for analysis a-01-N-262

---
+++ scripts/ngx/ngx_air_x1_doubleuppersplit.py
@@ -0,0 +1,56 @@
+#===============================================================================
+# EXTRACTION SCRIPT ngx_air_x1_doubleuppersplit.py
+#===============================================================================
+'''
+sensitivity_multiplier: 0.5
+modifier: 1
+eqtime: 30
+'''
+def main():
+    info('NGX Air Script')
+    open('D')
+    open('A')
+    close('H')
+    sleep(5)
+    open('G')
+    sleep(120)
+    close('G')
+    sleep(5)
+    if not analysis_type=='blank':
+        open('H')
+    sleep(60)
+    close('H')
+    close('D')
+    sleep(5)
+    open('G')
+    sleep(180)
+    close('A')
+    sleep(5)
+    open('D')
+    sleep(30)
+    close('D')
+    sleep(5)
+    open('A')
+    sleep(30)
+    close('A')
+    sleep(5)
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT ngx_pump_air.py
+#===============================================================================
+def main():
+	info('Pump after air analysis')
+	sleep(3)
+	open('D')
+	close('G')
+	open('A')
+	# gosub('jan:PumpMicrobone')
+	# gosub('jan:PumpMinibone')
+	#
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT ngx_pump_ms.py
+#===============================================================================
+def main():
+	info('Pumping spectrometer')
+	open(name='Z')
