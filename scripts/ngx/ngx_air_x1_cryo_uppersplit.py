1e6c99d50b7d171ef8c35f899748d62fb19c37aa
Stephen E Cox <cox.stephen.e@gmail.com>
2018-03-24 01:24:13 -0400

    repo updated for analysis ba-01-N-112

---
+++ scripts/ngx/ngx_air_x1_cryo_uppersplit.py
@@ -0,0 +1,67 @@
+#===============================================================================
+# EXTRACTION SCRIPT ngx_air_x1_cryo_uppersplit.py
+#===============================================================================
+'''
+sensitivity_multiplier: 0.5
+modifier: 1
+eqtime: 30
+'''
+def main():
+    info('NGX Air Script')
+    cryofocus = True
+    open('D')
+    close('H')
+    sleep(5)
+    open('G')
+    sleep(120)
+    close('G')
+    if cryofocus:
+        close('C')
+        gosub('CryoWaitFreeze')
+    sleep(5)
+    if not analysis_type=='blank':
+        open('H')
+    sleep(60)
+    close('H')
+    close('D')
+    sleep(5)
+    open('G')
+    sleep(180)
+    close('A')
+    if cryofocus:
+        open('C')
+        gosub('CryoWaitFreeze')
+        sleep(180)
+        close('A')
+        gosub('CryoWaitRelease')
+        sleep(300)
+    if not cryofocus:
+        close('A')
+
+#===============================================================================
+# POST EQUILIBRATION SCRIPT ngx_pump_air.py
+#===============================================================================
+def main():
+	info('Pump after air analysis')
+	cryofocus = True
+	sleep(3)
+	open('D')
+	if cryofocus:
+		open('A')
+		open('C')
+		gosub('CryoWaitPump')
+		sleep(120)
+		close('C')
+		set_cryo('freeze')
+	close('G')
+	open('A')
+	# gosub('jan:PumpMicrobone')
+	# gosub('jan:PumpMinibone')
+	#
+
+#===============================================================================
+# POST MEASUREMENT SCRIPT ngx_pump_ms.py
+#===============================================================================
+def main():
+	info('Pumping spectrometer')
+	open(name='Z')
