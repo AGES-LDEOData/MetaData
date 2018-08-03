35985d8dbb0e00936b904d84a36ae31d07f67ce3
Stephen E Cox <cox.stephen.e@gmail.com>
2018-08-03 13:20:15 -0400

    repo updated for analysis a-01-N-1122

--- scripts/ngx/ngx_air_x1.py
+++ scripts/ngx/ngx_air_x1.py
@@ -8,6 +8,12 @@ eqtime: 30
 '''
 def main():
     info('NGX Air Script')
+    p_test = get_pressure('Hub','IG1')
+    info('the value of p_test is ={}'.format(p_test))
+    if p_test > 1e-7:
+        cancel()
+    if p_test == 0:
+        cancel()
     cryofocus = False
     open('D')
     close('H')
