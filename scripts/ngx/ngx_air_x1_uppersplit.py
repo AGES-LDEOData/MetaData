2bddea23e98dca5daee5c59578f077fff3113369
Stephen E Cox <cox.stephen.e@gmail.com>
2018-08-09 07:00:40 -0400

    repo updated for analysis ba-01-N-538

--- scripts/ngx/ngx_air_x1_uppersplit.py
+++ scripts/ngx/ngx_air_x1_uppersplit.py
@@ -8,6 +8,12 @@ eqtime: 30
 '''
 def main():
     info('NGX Air Script')
+    p_test = get_pressure('Hub','IG1')
+    info('the value of p_test is ={}'.format(p_test))
+    if p_test > 1e-7:
+        cancel()
+    if p_test == 0:
+        cancel()
     open('D')
     open('A')
     close('H')
@@ -23,7 +29,7 @@ def main():
     close('D')
     sleep(5)
     open('G')
-    sleep(180)
+    sleep(cleanup)
     close('A')
     sleep(5)
 
