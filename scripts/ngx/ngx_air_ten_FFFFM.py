6ed1cb144c68650e14ecb0664c4809a781987484
AGES-007 <ldeo.ages.007@gmail.com>
2021-12-14 12:27:45 -0500

    repo updated for analysis a-03-N-577

--- scripts/ngx/ngx_air_ten_FFFFM.py
+++ scripts/ngx/ngx_air_ten_FFFFM.py
@@ -98,7 +98,7 @@ def main():
 
     set_integration_time(1)
     if mx.peakcenter.after:
-        activate_detectors(ACTIVE_DETECTORS, peak_center=True)
+        activate_detectors(*ACTIVE_DETECTORS, peak_center=True)
         peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope, integration_time=1, config_name=mx.peakcenter.config_name)
     info('finished measure script')
 
